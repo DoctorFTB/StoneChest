f704e25af32606906a7d4aa04f6f557eb7a382f9
FTB_lag <tabolskyy.git@gmail.com>
2024-06-02 22:22:26 +0300

    1.20.4 neo port

---
+++ src/main/java/ftblag/stonechest/SCEventHandler.java
@@ -0,0 +1,20 @@
+package ftblag.stonechest;
+
+import ftblag.stonechest.client.TileEntityStoneChestRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+@OnlyIn(Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = StoneChest.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SCEventHandler {
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void doClientStuff(FMLClientSetupEvent event) {
+        BlockEntityRenderers.register(SCRegistry.CHEST_TILE_TYPE.get(), TileEntityStoneChestRenderer::new);
+    }
+}
