360708837292ea0d35f6bd3b5d84c2067b9eab40
FTB_lag <tabolskyy.git@gmail.com>
2024-12-15 03:35:21 +0300

    1.21.4 port

---
+++ src/main/java/ftblag/stonechest/blocks/BlockStoneChest.java
@@ -0,0 +1,29 @@
+package ftblag.stonechest.blocks;
+
+import ftblag.stonechest.SCRegistry;
+import ftblag.stonechest.tileentities.TileEntityStoneChest;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class BlockStoneChest extends ChestBlock {
+    private final EnumStoneChest chestType;
+
+    public BlockStoneChest(EnumStoneChest chestType, ResourceKey<Block> key) {
+        super(() -> SCRegistry.CHEST_TILE_TYPE.get(), Properties.of().strength(2.5F).sound(SoundType.STONE).setId(key));
+        this.chestType = chestType;
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new TileEntityStoneChest(this.chestType, pos, state);
+    }
+
+    public EnumStoneChest getChestType() {
+        return this.chestType;
+    }
+}
---
+++ src/main/java/ftblag/stonechest/blocks/EnumStoneChest.java
@@ -0,0 +1,16 @@
+package ftblag.stonechest.blocks;
+
+public enum EnumStoneChest {
+      ANDESITE,
+      CALCITE,
+      COBBLED_DEEPSLATE,
+      COBBLESTONE,
+      DEEPSLATE,
+      DIORITE,
+      GRANITE,
+      PRISMARINE,
+      STONE,
+      TUFF;
+
+      public static final EnumStoneChest[] VALUES = values();
+}
