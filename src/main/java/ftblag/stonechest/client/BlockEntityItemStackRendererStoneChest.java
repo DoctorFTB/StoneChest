d2dd68752bd913379131e8fa8f59d015632e5a85
FTB_lag <tabolskyy.git@gmail.com>
2026-01-08 06:28:25 +0300

    1.20.4 update

---
+++ src/main/java/ftblag/stonechest/client/BlockEntityItemStackRendererStoneChest.java
@@ -0,0 +1,47 @@
+package ftblag.stonechest.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import ftblag.stonechest.SCRegistry;
+import ftblag.stonechest.blocks.BlockStoneChest;
+import ftblag.stonechest.blocks.EnumStoneChest;
+import ftblag.stonechest.blockentities.BlockEntityStoneChest;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class BlockEntityItemStackRendererStoneChest extends BlockEntityWithoutLevelRenderer {
+    public static final BlockEntityItemStackRendererStoneChest INSTANCE = new BlockEntityItemStackRendererStoneChest();
+
+    private BlockEntityStoneChest[] entities = new BlockEntityStoneChest[EnumStoneChest.VALUES.length];
+
+    {
+        for (EnumStoneChest type : EnumStoneChest.VALUES)
+            entities[type.ordinal()] = new BlockEntityStoneChest(type, BlockPos.ZERO, SCRegistry.chests[type.ordinal()].get().defaultBlockState());
+    }
+
+    public BlockEntityItemStackRendererStoneChest() {
+        super(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
+    }
+
+    @Override
+    public void onResourceManagerReload(ResourceManager manager) {
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay) {
+        Block block = Block.byItem(stack.getItem());
+        if (block instanceof BlockStoneChest) {
+            Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(this.entities[((BlockStoneChest)block).getChestType().ordinal()], poseStack, buffer, packedLight, packedOverlay);
+        } else {
+            super.renderByItem(stack, displayContext, poseStack, buffer, packedLight, packedOverlay);
+        }
+    }
+}
